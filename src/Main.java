1d422816a05ecb1f5b7d13d5829da55a4be90d08
AlexM <wmatallana28@gmail.com>
2025-08-26 15:52:13 -0500

    Trabajo colaborativo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+     // Por estudiante: feature/XX-nombreTarea, con XX de 01 a 12.
 
         Scanner sc = new Scanner(System.in);
         int opcion;
